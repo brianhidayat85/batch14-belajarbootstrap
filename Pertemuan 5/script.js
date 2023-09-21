40b5709fb5d6bd62bb6f97997d5e2fac0c6f10d7
brianhidayat85 <brianhidayat85@gmail.com>
2023-09-21 21:16:28 +0700

    pesan commit

--- Pertemuan 5/script.js
+++
@@ -1,7 +0,0 @@
-let nama = prompt("Masukkan nama Anda: ");
-
-// menggabungkan string dengan variabel menggunakan simbol "+"      => (concat / concatenation)
-document.write("Selamat datang, <b>" + nama + "</b>");
-
-// menunjuk elemen html dengan id running-text
-document.getElementById('running-text').innerHTML = "<marquee>Pengumuman: Hari ini pertemuan ke-5 kelas Belajar Langkah Fundamental Untuk Menjadi Web Programmer Hacktiv8! Keep spirit!</marquee>";
